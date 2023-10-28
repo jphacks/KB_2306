80551f12ceb7d99cc76d79c7df7afa1e350cf12a
Kentaro Ishikawa <kentaroishikawa1286@gmail.com>
2023-10-28 12:18:39 +0900

    音声ファイルを Gradio で出力

--- huggingface/app.py
+++ huggingface/app.py
@@ -20,13 +20,20 @@ def execute(whisper_model_name, audiofile):
     # (2, T) numpy arrays
     Y, V = separator.separate(X)
 
+    # Save the separated audio sources to files
+    accompaniment_file_path = 'accompaniment.wav'
+    vocals_file_path = 'vocals.wav'
+    sf.write(accompaniment_file_path, Y.T, sample_rate)
+    sf.write(vocals_file_path, V.T, sample_rate)
+
     # Create a whisper instance
     whisper_model = stable_whisper.load_model(whisper_model_name)
 
     # Transcribe the vocals with Whisper
     segments = transcribe(whisper_model, V.T, sample_rate)
 
-    return segments
+    # Return the file paths for download
+    return accompaniment_file_path, vocals_file_path, segments
 
 # Create a dropdown to select the model
 model_options = ["base", "small", "medium", "large", "large-v2"]
@@ -39,7 +46,11 @@ audiofile = Audio(type='filepath', label="Upload an audio file")
 iface = gr.Interface(
     fn=lambda whisper_model_name, audiofile: execute(whisper_model_name, audiofile),
     inputs=[model, audiofile],
-    outputs=gr.outputs.JSON()
+    outputs=[
+        gr.outputs.Audio(label="Accompaniment"),
+        gr.outputs.Audio(label="Vocals"),
+        gr.outputs.JSON(label="Transcription")
+    ]
 )
 
 # Launch the interface
