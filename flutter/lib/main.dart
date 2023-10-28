9222705e8ab6c0bf890f7b37ee3ba02f95cbe0b2
serori521 <kichirou516@icloud.com>
2023-10-28 07:37:47 +0000

    一旦

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -8,7 +8,8 @@ import 'const.dart';
 
 // main関数: アプリのエントリーポイントです。アプリの起動時に最初に実行される関数です。
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+
+  runApp(MaterialApp(theme: ThemeData.dark(), home: MyApp()));
 }
 
 // MyAppクラス: Flutterアプリケーションのルートウィジェットです。
@@ -38,7 +39,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     super.initState();
   }
 
-// Scaffoldウィジェット: 基本的なアプリの構造を提供するウィジェットです。
+  // Scaffoldウィジェット: 基本的なアプリの構造を提供するウィジェットです。
   @override
   Widget build(BuildContext context) {
     // 画面の幅を取得
@@ -49,7 +50,6 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     var extTextSize = mainTextSize * 0.8;
     // 計算されたmainTextSizeをlyricUI.defaultSizeに設定
     lyricUI.defaultSize = mainTextSize;
-    extTextSize = extTextSize;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Plugin example app'),
@@ -58,18 +58,46 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     );
   }
 
+  //曲名のリスト
+  List<Widget> buildSongList() {
+    return [
+      ListTile(title: Text("Song 1: Sample Title")),
+      ListTile(title: Text("Song 2: Another Title")),
+      ListTile(title: Text("Song 3: More Music")),
+      ListTile(title: Text("Song 4: Melody Tune")),
+      ListTile(title: Text("Song 5: Final Song"))
+    ];
+  }
+  //全体の見た目をつかさどる
   Widget buildContainer() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Row(
       children: [
-        buildReaderWidget(),
+        //左側曲リスト
+        Container(
+          color: Colors.grey,
+          width: 250,  // Width for song list
+          child: ListView(
+            children: buildSongList(),
+          ),
+        ),
+        //右側　歌詞表示　再生操作類
         Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ...buildPlayControl(),
-              ],
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //歌詞表示
+              buildReaderWidget(),
+              //再生操作類の作成
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ...buildPlayControl(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
@@ -77,7 +105,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   }
 
   var lyricPadding = 40.0;
-//歌詞表示部分
+  //歌詞表示部分
   Stack buildReaderWidget() {
     return Stack(
       children: [
@@ -88,12 +116,14 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
           position: playProgress,
           lyricUi: lyricUI,
           playing: playing,
-          size: Size(double.infinity, MediaQuery.of(context).size.height / 2),
-          emptyBuilder: () => Center(
-            child: Text(
-              "No lyrics",
-              style: lyricUI.getOtherMainTextStyle(),
-            ),
+          size: Size(double.infinity, MediaQuery.of(context).size.height * 0.75),
+          emptyBuilder: () => Container(height: MediaQuery.of(context).size.height * 0.75,
+          
+              child: Text(
+                "No lyrics",
+                style: lyricUI.getOtherMainTextStyle(),
+              ),
+            
           ),
           selectLineBuilder: (progress, confirm) {
             return Row(
@@ -121,44 +151,58 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
               ],
             );
           },
-        )
+        ),
       ],
     );
   }
-
+  //再生コントロール　部分
   List<Widget> buildPlayControl() {
     return [
       Container(
-        height: 20,
+        height: 15,
       ),
+      //進行バーを最大値ではないとき表示
       if (sliderProgress < max_value)
-        Slider(
-          min: 0,
-          max: max_value,
-          label: sliderProgress.toString(),
-          value: sliderProgress,
-          activeColor: Colors.blueGrey,
-          inactiveColor: Colors.blue,
-          onChanged: (double value) {
-            setState(() {
-              sliderProgress = value;
-            });
-          },
-          onChangeStart: (double value) {
-            isTap = true;
-          },
-          onChangeEnd: (double value) {
-            isTap = false;
-            setState(() {
-              playProgress = value.toInt();
-            });
-            audioPlayer?.seek(Duration(milliseconds: value.toInt()));
-          },
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //進行バー
+            Expanded(  
+              child: Slider(
+                min: 0,
+                max: max_value,
+                label: sliderProgress.toString(),
+                value: sliderProgress,
+                activeColor: Colors.blueGrey,
+                inactiveColor: Colors.blue,
+                onChanged: (double value) {
+                  setState(() {
+                    sliderProgress = value;
+                  });
+                },
+                onChangeStart: (double value) {
+                  isTap = true;
+                },
+                onChangeEnd: (double value) {
+                  isTap = false;
+                  setState(() {
+                    playProgress = value.toInt();
+                  });
+                  audioPlayer?.seek(Duration(milliseconds: value.toInt()));
+                },
+              ),
+            ),
+            //時間のテキスト
+            Text(
+              "${_formatDuration(Duration(milliseconds: playProgress))}/${_formatDuration(Duration(milliseconds: max_value.toInt()))}"
+            ),
+          ],
         ),
+      //再生戻る進むボタン
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          //前の曲に戻る
+          //前の曲に戻る 色や機能変更必要
           ElevatedButton(
             onPressed: () {
               // 前の曲に関する処理をここに追加します。
@@ -174,39 +218,40 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
           //一時停止再生
           ElevatedButton(
             onPressed: () async {
-              if (audioPlayer == null || !playing) {
-                audioPlayer = AudioPlayer()..play(AssetSource("music1.mp3"));
-                setState(() {
-                  playing = true;
-                });
-                audioPlayer?.onDurationChanged.listen((Duration event) {
-                  setState(() {
-                    max_value = event.inMilliseconds.toDouble();
+              if (!playing) {
+                // 初めての再生の場合のみイベントリスナーを設定
+                if (max_value == null) {
+                  audioPlayer.onDurationChanged.listen((Duration event) {
+                    setState(() {
+                      max_value = event.inMilliseconds.toDouble();
+                    });
                   });
-                });
-                audioPlayer?.onPositionChanged.listen((Duration event) {
-                  if (isTap) return;
-                  setState(() {
-                    sliderProgress = event.inMilliseconds.toDouble();
-                    playProgress = event.inMilliseconds;
+                  audioPlayer.onPositionChanged.listen((Duration event) {
+                    if (isTap) return;
+                    setState(() {
+                      sliderProgress = event.inMilliseconds.toDouble();
+                      playProgress = event.inMilliseconds;
+                    });
                   });
-                });
-
-                audioPlayer?.onPlayerStateChanged.listen((PlayerState state) {
-                  setState(() {
-                    playing = state == PlayerState.playing;
+                  audioPlayer.onPlayerStateChanged.listen((PlayerState state) {
+                    setState(() {
+                      playing = state == PlayerState.playing;
+                    });
                   });
-                });
-              } else {
-                if (playing) {
-                  audioPlayer?.pause();
-                } else {
-                  audioPlayer?.resume();
                 }
-                setState(() {
-                  playing = !playing;
-                });
+                // 曲を再生
+                await audioPlayer.play(AssetSource("music1.mp3"));
+              } else {
+                // 一時停止または再開
+                if (playing) {
+                  audioPlayer.pause();
+                } else {
+                  audioPlayer.resume();
+                }
               }
+              setState(() {
+                playing = !playing;
+              });
             },
             style: ElevatedButton.styleFrom(
               primary: playing ? Colors.grey : Colors.white,
@@ -235,7 +280,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   }
 
   var playing = false;
-// 背景の設定
+  // 歌詞部分の背景の設定
   List<Widget> buildReaderBackground() {
     return [
       Positioned.fill(
@@ -244,24 +289,16 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
             gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
-              colors: [Colors.blue, Colors.purple], //青から紫へのグラデーション
+              colors: [Colors.blue, Colors.purple], //青から紫へのグラデーション変更必須
             ),
           ),
         ),
       ),
-      Positioned.fill(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: Container(
-            color: Colors.black.withOpacity(0.3),
-          ),
-        ),
-      )
     ];
   }
 
 
-  var extTextSize = 16.0;
+
   var lineGap = 16.0;
   var inlineGap = 10.0;
   var lyricAlign = LyricAlign.CENTER;
@@ -277,4 +314,11 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
 
   Text buildTitle(String title) => Text(title,
       style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green));
-}
+  }
+  // Helper function to format milliseconds into mm:ss format
+  String _formatDuration(Duration duration) {
+    var minutes = duration.inMinutes;
+    var seconds = duration.inSeconds % 60;
+    return "${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
+  }
+                 
