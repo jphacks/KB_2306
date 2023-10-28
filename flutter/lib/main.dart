b2642a722e6402a90ea9b0593b10de495d897a1b
serori521 <kichirou516@icloud.com>
2023-10-28 03:48:16 +0000

    sample_UIをある程度作成しました

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,84 +1,501 @@
-import 'dart:async';
+import 'dart:ui';
 
-import 'package:firebase_core/firebase_core.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase/firebase_options.dart';
+import 'package:flutter_lyric/lyrics_reader.dart';
 
-Future<void> main() async {
-  await runZonedGuarded(
-    () async {
-      WidgetsFlutterBinding.ensureInitialized();
+import 'const.dart';
 
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-
-      runApp(const MyApp());
-    },
-    (error, stackTrace) {
-      print('runZonedGuarded: Caught error in my root zone.');
-      print('error\n$error');
-      print('stacktrace\n$stackTrace');
-    },
-  );
+// main関数: アプリのエントリーポイントです。アプリの起動時に最初に実行される関数です。
+void main() {
+  runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// MyAppクラス: Flutterアプリケーションのルートウィジェットです。
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  AudioPlayer? audioPlayer;
+  double sliderProgress = 111658;
+  int playProgress = 111658;
+  double max_value = 211658;
+  bool isTap = false;
+
+  bool useEnhancedLrc = false;
+  var lyricModel = LyricsModelBuilder.create()
+      .bindLyricToMain(normalLyric)
+      .bindLyricToExt(transLyric)
+      .getModel();
+
+  var lyricUI = UINetease();
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void initState() {
+    super.initState();
   }
 
+// Scaffoldウィジェット: 基本的なアプリの構造を提供するウィジェットです。
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: buildContainer(),
+    );
+  }
+
+  Widget buildContainer() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildReaderWidget(),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ...buildPlayControl(),
+                ...buildUIControl(),
+              ],
+            ),
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
+      ],
+    );
+  }
+
+  var lyricPadding = 40.0;
+//歌詞表示部分
+  Stack buildReaderWidget() {
+    return Stack(
+      children: [
+        ...buildReaderBackground(), //背景の設定
+        LyricsReader(
+          padding: EdgeInsets.symmetric(horizontal: lyricPadding),
+          model: lyricModel,
+          position: playProgress,
+          lyricUi: lyricUI,
+          playing: playing,
+          size: Size(double.infinity, MediaQuery.of(context).size.height / 2),
+          emptyBuilder: () => Center(
+            child: Text(
+              "No lyrics",
+              style: lyricUI.getOtherMainTextStyle(),
+            ),
+          ),
+          selectLineBuilder: (progress, confirm) {
+            return Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      LyricsLog.logD("点击事件");
+                      confirm.call();
+                      setState(() {
+                        audioPlayer?.seek(Duration(milliseconds: progress));
+                      });
+                    },
+                    icon: Icon(Icons.play_arrow, color: Colors.green)),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.green),
+                    height: 1,
+                    width: double.infinity,
+                  ),
+                ),
+                Text(
+                  progress.toString(),
+                  style: TextStyle(color: Colors.green),
+                )
+              ],
+            );
+          },
+        )
+      ],
+    );
+  }
+
+  List<Widget> buildPlayControl() {
+    return [
+      Container(
+        height: 20,
+      ),
+      if (sliderProgress < max_value)
+        Slider(
+          min: 0,
+          max: max_value,
+          label: sliderProgress.toString(),
+          value: sliderProgress,
+          activeColor: Colors.blueGrey,
+          inactiveColor: Colors.blue,
+          onChanged: (double value) {
+            setState(() {
+              sliderProgress = value;
+            });
+          },
+          onChangeStart: (double value) {
+            isTap = true;
+          },
+          onChangeEnd: (double value) {
+            isTap = false;
+            setState(() {
+              playProgress = value.toInt();
+            });
+            audioPlayer?.seek(Duration(milliseconds: value.toInt()));
+          },
         ),
-      );
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //前の曲に戻る
+          ElevatedButton(
+            onPressed: () {
+              // 前の曲に関する処理をここに追加します。
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              onPrimary: Colors.grey,
+              elevation: 5,
+              shape: CircleBorder(),
+            ),
+            child: Icon(Icons.skip_previous),
+          ),
+          //一時停止再生
+          ElevatedButton(
+            onPressed: () async {
+              if (audioPlayer == null || !playing) {
+                audioPlayer = AudioPlayer()..play(AssetSource("music1.mp3"));
+                setState(() {
+                  playing = true;
+                });
+                audioPlayer?.onDurationChanged.listen((Duration event) {
+                  setState(() {
+                    max_value = event.inMilliseconds.toDouble();
+                  });
+                });
+                audioPlayer?.onPositionChanged.listen((Duration event) {
+                  if (isTap) return;
+                  setState(() {
+                    sliderProgress = event.inMilliseconds.toDouble();
+                    playProgress = event.inMilliseconds;
+                  });
+                });
+
+                audioPlayer?.onPlayerStateChanged.listen((PlayerState state) {
+                  setState(() {
+                    playing = state == PlayerState.playing;
+                  });
+                });
+              } else {
+                if (playing) {
+                  audioPlayer?.pause();
+                } else {
+                  audioPlayer?.resume();
+                }
+                setState(() {
+                  playing = !playing;
+                });
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              primary: playing ? Colors.grey : Colors.white,
+              onPrimary: playing ? Colors.white : Colors.grey,
+              elevation: playing ? 0 : 5,
+              shape: CircleBorder(),
+            ),
+            child: Icon(playing ? Icons.pause : Icons.play_arrow),
+          ),
+          //次の曲
+          ElevatedButton(
+            onPressed: () {
+              // 次の曲に関する処理をここに追加します。
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              onPrimary: Colors.grey,
+              elevation: 5,
+              shape: CircleBorder(),
+            ),
+            child: Icon(Icons.skip_next),
+          ),
+        ],
+      )
+    ];
+  }
+
+  var playing = false;
+// 背景の設定
+  List<Widget> buildReaderBackground() {
+    return [
+      Positioned.fill(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue, Colors.purple], //青から紫へのグラデーション
+            ),
+          ),
+        ),
+      ),
+      Positioned.fill(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            color: Colors.black.withOpacity(0.3),
+          ),
+        ),
+      )
+    ];
+  }
+
+  var mainTextSize = 18.0;
+  var extTextSize = 16.0;
+  var lineGap = 16.0;
+  var inlineGap = 10.0;
+  var lyricAlign = LyricAlign.CENTER;
+  var highlightDirection = HighlightDirection.LTR;
+
+  List<Widget> buildUIControl() {
+    return [
+      Container(
+        height: 30,
+      ),
+      Text("UI setting", style: TextStyle(fontWeight: FontWeight.bold)),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Checkbox(
+              value: lyricUI.enableHighlight(),
+              onChanged: (value) {
+                setState(() {
+                  lyricUI.highlight = (value ?? false);
+                  refreshLyric();
+                });
+              }),
+          Text("enable highLight"),
+          Checkbox(
+              value: useEnhancedLrc,
+              onChanged: (value) {
+                setState(() {
+                  useEnhancedLrc = value!;
+                  lyricModel = LyricsModelBuilder.create()
+                      .bindLyricToMain(value ? advancedLyric : normalLyric)
+                      .bindLyricToExt(transLyric)
+                      .getModel();
+                });
+              }),
+          Text("use Enhanced lrc")
+        ],
+      ),
+      buildTitle("highlight direction"),
+      Row(
+        mainAxisSize: MainAxisSize.min,
+        children: HighlightDirection.values
+            .map(
+              (e) => Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Radio<HighlightDirection>(
+                        activeColor: Colors.orangeAccent,
+                        value: e,
+                        groupValue: highlightDirection,
+                        onChanged: (v) {
+                          setState(() {
+                            highlightDirection = v!;
+                            lyricUI.highlightDirection = highlightDirection;
+                            refreshLyric();
+                          });
+                        }),
+                    Text(e.toString().split(".")[1])
+                  ],
+                ),
+              )),
+            )
+            .toList(),
+      ),
+      buildTitle("lyric padding"),
+      Slider(
+        min: 0,
+        max: 100,
+        label: lyricPadding.toString(),
+        value: lyricPadding,
+        activeColor: Colors.blueGrey,
+        inactiveColor: Colors.blue,
+        onChanged: (double value) {
+          setState(() {
+            lyricPadding = value;
+          });
+        },
+      ),
+      buildTitle("lyric primary text size"),
+      Slider(
+        min: 15,
+        max: 30,
+        label: mainTextSize.toString(),
+        value: mainTextSize,
+        activeColor: Colors.blueGrey,
+        inactiveColor: Colors.blue,
+        onChanged: (double value) {
+          setState(() {
+            mainTextSize = value;
+          });
+        },
+        onChangeEnd: (double value) {
+          setState(() {
+            lyricUI.defaultSize = mainTextSize;
+            refreshLyric();
+          });
+        },
+      ),
+      buildTitle("lyric secondary text size"),
+      Slider(
+        min: 15,
+        max: 30,
+        label: extTextSize.toString(),
+        value: extTextSize,
+        activeColor: Colors.blueGrey,
+        inactiveColor: Colors.blue,
+        onChanged: (double value) {
+          setState(() {
+            extTextSize = value;
+          });
+        },
+        onChangeEnd: (double value) {
+          setState(() {
+            lyricUI.defaultExtSize = extTextSize;
+            refreshLyric();
+          });
+        },
+      ),
+      buildTitle("lyric line spacing"),
+      Slider(
+        min: 10,
+        max: 80,
+        label: lineGap.toString(),
+        value: lineGap,
+        activeColor: Colors.blueGrey,
+        inactiveColor: Colors.blue,
+        onChanged: (double value) {
+          setState(() {
+            lineGap = value;
+          });
+        },
+        onChangeEnd: (double value) {
+          setState(() {
+            lyricUI.lineGap = lineGap;
+            refreshLyric();
+          });
+        },
+      ),
+      buildTitle("primary and secondary lyric spacing"),
+      Slider(
+        min: 10,
+        max: 80,
+        label: inlineGap.toString(),
+        value: inlineGap,
+        activeColor: Colors.blueGrey,
+        inactiveColor: Colors.blue,
+        onChanged: (double value) {
+          setState(() {
+            inlineGap = value;
+          });
+        },
+        onChangeEnd: (double value) {
+          setState(() {
+            lyricUI.inlineGap = inlineGap;
+            refreshLyric();
+          });
+        },
+      ),
+      buildTitle("select line bias"),
+      Slider(
+        min: 0.3,
+        max: 0.8,
+        label: bias.toString(),
+        value: bias,
+        activeColor: Colors.blueGrey,
+        inactiveColor: Colors.blue,
+        onChanged: (double value) {
+          setState(() {
+            bias = value;
+          });
+        },
+        onChangeEnd: (double value) {
+          setState(() {
+            lyricUI.bias = bias;
+            refreshLyric();
+          });
+        },
+      ),
+      buildTitle("lyric align"),
+      Row(
+        mainAxisSize: MainAxisSize.min,
+        children: LyricAlign.values
+            .map(
+              (e) => Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Radio<LyricAlign>(
+                        activeColor: Colors.orangeAccent,
+                        value: e,
+                        groupValue: lyricAlign,
+                        onChanged: (v) {
+                          setState(() {
+                            lyricAlign = v!;
+                            lyricUI.lyricAlign = lyricAlign;
+                            refreshLyric();
+                          });
+                        }),
+                    Text(e.toString().split(".")[1])
+                  ],
+                ),
+              )),
+            )
+            .toList(),
+      ),
+      buildTitle("select line base"),
+      Row(
+        children: LyricBaseLine.values
+            .map((e) => Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Radio<LyricBaseLine>(
+                            activeColor: Colors.orangeAccent,
+                            value: e,
+                            groupValue: lyricBiasBaseLine,
+                            onChanged: (v) {
+                              setState(() {
+                                lyricBiasBaseLine = v!;
+                                lyricUI.lyricBaseLine = lyricBiasBaseLine;
+                                refreshLyric();
+                              });
+                            }),
+                        Text(e.toString().split(".")[1])
+                      ],
+                    ),
+                  ),
+                ))
+            .toList(),
+      ),
+    ];
+  }
+
+  void refreshLyric() {
+    lyricUI = UINetease.clone(lyricUI);
+  }
+
+  var bias = 0.5;
+  var lyricBiasBaseLine = LyricBaseLine.CENTER;
+
+  Text buildTitle(String title) => Text(title,
+      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green));
 }
